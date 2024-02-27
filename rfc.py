2b32e23557d4d7f97e6885bbbf442a2a71565efe
LuoTailong <40262839+LuoTailong@users.noreply.github.com>
2024-02-27 11:39:36 -0800

    Add files via upload

--- rfc.py
+++ rfc.py
@@ -48,21 +48,53 @@ def decrypt_rfc(cipher, key):
 
         indx = 0
         for i, j in ((i, j) for i in range(d) for j in range(cipher_length)
-             if matrix[i][j] == '*' and indx < len(cipher)): matrix[i][j], indx = cipher[indx], indx + 1
+                     if matrix[i][j] == '*' and indx < len(cipher)): matrix[i][j], indx = cipher[indx], indx + 1
 
         result = find_direction(0, d, matrix, cipher_length, 1)
         cipher = "".join(result)
     return cipher
 
 
-# Test 1 Encryption
-key_en = (4, 5)  # Depth of 4, repeat 5 times
-text = "CRYPTOLOGY IS THE PRACTICE AND STUDY OF TECHNIQUES FOR SECURE COMMUNICATION IN THE PRESENCE OF THIRD PARTIES CALLED ADVERSARIES"
-encrypted_text = encrypt_rfc(text, key_en)
-print("Encrypted Text:", encrypted_text)
+while True:
+    print('------------------------------------------------------')
+    print("Rail Fence Cipher Menu Options")
+    print('------------------------------------------------------')
+    print('1. Encrypt Text')
+    print('2. Decrypt Text')
+    print('3. Test 1 (Encryption)')
+    print('4. Test 2 (Decryption)')
+    print('5. Quit')
+    option = int(input())
 
-# Test 2 Decryption
-key_de = (3, 3)  # Depth of 3, repeat 3 times
-encrypted_message = "TPSNIONFRMHANOIREEOIBTSEAKLAPSEHISOBPEGTBRQREPTTMHRTHTTAWE AACTFVAECAOLHANSEEKFHALOITUEEAICNLEYOLTOLEPADFKATATSJMSIINSH ROCTITRIEEAKYNHYUEOTTSTATSIIRSARERUYUEDPCLETEROINEIYEHC"
-decrypted_text = decrypt_rfc(encrypted_message.replace(" ", ""), key_de)
-print("Decrypted Text:", decrypted_text)
+    if option == 1:
+        pt = input("Please enter the plain text:\n")
+        depth = int(input("Please enter the depth of encryption:\n"))
+        round = int(input("Please enter the number of encrypt round:\n"))
+        encrypted_text = encrypt_rfc(pt, (depth, round))
+        print("Encrypted Text:", encrypted_text)
+    elif option == 2:
+        ct = input("Please enter the cipher text:\n")
+        depth = int(input("Please enter the depth of decryption:\n"))
+        round = int(input("Please enter the number of decrypt round:\n"))
+        decrypted_text = encrypt_rfc(ct, (depth, round))
+        print("Decrypted Text:", decrypted_text)
+    elif option == 3:
+        # Test 1 Encryption
+        key_en = (4, 5)  # Depth of 4, repeat 5 times
+        text = "CRYPTOLOGY IS THE PRACTICE AND STUDY OF TECHNIQUES FOR SECURE COMMUNICATION IN THE PRESENCE OF THIRD PARTIES CALLED ADVERSARIES"
+        encrypted_text = encrypt_rfc(text, key_en)
+        print("Encrypted Text:", encrypted_text)
+    elif option == 4:
+        # Test 2 Decryption
+        key_de = (3, 3)  # Depth of 3, repeat 3 times
+        encrypted_message = "TPSNIONFRMHANOIREEOIBTSEAKLAPSEHISOBPEGTBRQREPTTMHRTHTTAWE AACTFVAECAOLHANSEEKFHALOITUEEAICNLEYOLTOLEPADFKATATSJMSIINSH ROCTITRIEEAKYNHYUEOTTSTATSIIRSARERUYUEDPCLETEROINEIYEHC"
+        decrypted_text = decrypt_rfc(encrypted_message.replace(" ", ""), key_de)
+        print("Decrypted Text:", decrypted_text)
+    elif option == 5:
+        print("Good bye")
+        break
+    else:
+        print("Wrong option")
+
+
+
